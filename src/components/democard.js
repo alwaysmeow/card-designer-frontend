0ae641daf211664940b8f1f4b934f9df18e181a1
Sergey Krivtsov <130004663+alwaysmeow@users.noreply.github.com>
2024-06-25 01:09:29 +0300

    image load fix

--- src/components/democard.js
+++ src/components/democard.js
@@ -6,7 +6,7 @@ import crop from '../tools/crop';
 function DemoCard () {
     const image = useSelector((state) => state.crop.image)
     const cropData = useSelector((state) => state.crop.cropData)
-    const [src, setSrc] = useState()
+    const [src, setSrc] = useState(null)
     
     useEffect(() => {
         crop(image, cropData)
@@ -19,11 +19,11 @@ function DemoCard () {
 
     return (
         <div className="flex-col aspect-card justify-between w-80 mx-auto p-5 border-2 border-black rounded-xl"
-            style={{
+            style={src ? {
                 backgroundImage: `url(${src})`, 
                 backgroundSize: 'cover', 
                 backgroundPosition: 'center',
-             }}
+             } : {}}
         >
             <div className="h-1/2">
 
--- src/tools/crop.js
+++ src/tools/crop.js
@@ -1,30 +1,33 @@
 function crop(src, data) {
     return new Promise((resolve, reject) => {
-        const image = new Image();
-        image.src = src;
+        if (src && data)
+        {
+            const image = new Image();
+            image.src = src;
+        
+            image.onload = () => {
+                const canvas = document.createElement('canvas');
+                const ctx = canvas.getContext('2d');
+            
+                canvas.width = data.width;
+                canvas.height = data.height;
+            
+                ctx.drawImage(
+                    image,
+                    data.y, data.x, data.width, data.height,
+                    0, 0, data.width, data.height
+                );
+            
+                resolve(canvas.toDataURL('image/png'));
+            }
     
-        image.onload = () => {
-            const canvas = document.createElement('canvas');
-            const ctx = canvas.getContext('2d');
-        
-            canvas.width = data.width;
-            canvas.height = data.height;
-        
-            ctx.drawImage(
-                image,
-                data.y, data.x, data.width, data.height,
-                0, 0, data.width, data.height
-            );
-        
-            resolve(canvas.toDataURL('image/png'));
+            image.onerror = (error) => {
+                reject(error);
+            };
         }
-
-        image.onerror = (error) => {
-            console.log(error);
-            reject(error);
-        };
+        else
+            reject();
     })
-    
 }
 
 export default crop
